45acb1e1c9aa607bd448851406d4f6d564e28393
Ben <ben@wainei.net>
2015-08-17 23:06:02 +0200

    better naming here

---
+++ app/scripts/app.js
@@ -0,0 +1,34 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name twitchcancer
+ * @description
+ * # Live monitoring of Twitch chats cancer level via web-sockets.
+ *
+ * Main module of the application.
+ */
+angular
+  .module('twitchCancer', [
+    'ngAnimate',
+    'ngCookies',
+    'ngMessages',
+    'ngResource',
+    'ngRoute',
+    'ngSanitize',
+    'ngTouch',
+    'ngWebSocket',
+    'ui.bootstrap',
+    'twitchProfile'
+  ])
+  .config(function ($routeProvider) {
+    $routeProvider
+      .when('/', {
+        templateUrl: 'views/main.html',
+        controller: 'MainCtrl',
+        controllerAs: 'main'
+      })
+      .otherwise({
+        redirectTo: '/'
+      });
+  });
