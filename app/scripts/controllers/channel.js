46852019c9247bff0486b9f6659eb330a2dc6bb0
Ben <ben@wainei.net>
2015-08-26 19:51:40 +0200

    added a proper channel page

--- app/scripts/controllers/channel.js
+++ app/scripts/controllers/channel.js
@@ -7,17 +7,36 @@
  * # ChannelCtrl
  * Controller of the channel view
  */
-angular.module('controllers.channel', ['api.websocket'])
-  .controller('ChannelCtrl', function($scope, $routeParams, api) {
-      $scope.channelName = $routeParams.channelName;
+angular.module('controllers.channel', ['countTo', 'ordinal', 'api.websocket', 'twitchProfile'])
+  .controller('ChannelCtrl', function($scope, $routeParams, api, twitch_profiles) {
+    $scope.channelName = $routeParams.channelName;
 
-      api.subscribe("twitchcancer.channel.#"+$scope.channelName, function(json) {
-        $scope.stats = json;
-      });
+    $scope.$watch(function() {
+        return twitch_profiles.load($scope.channelName);
+      },
+      function(newValue) {
+        if (newValue) {
+          $scope.profile = newValue;
+        }
+      }
+    );
 
-      $scope.$on("$destroy", function() {
-        api.unsubscribe("twitchcancer.channel.#"+$scope.channelName);
-      });
+    api.subscribe("twitchcancer.channel.#"+$scope.channelName, function(json) {
+      $scope.stats = json;
+    });
+
+    $scope.$on("$destroy", function() {
+      api.unsubscribe("twitchcancer.channel.#"+$scope.channelName);
+    });
+  })
+  .directive('rankBadge', function() {
+    return {
+      restrict: 'A',
+      replace: true,
+      scope: {
+        rank: '='
+      },
+      template: '<span class="badge rank-{{rank}}">{{rank | ordinal}}</span>'
     }
-  )
+  })
 ;
