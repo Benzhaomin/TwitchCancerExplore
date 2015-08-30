70132d26b64a844da0d08506c319fdd9ce6990ad
Ben <ben@wainei.net>
2015-08-30 02:39:36 +0200

    added stats from the back-end to the about page

--- app/scripts/controllers/about.js
+++ app/scripts/controllers/about.js
@@ -7,8 +7,16 @@
  * # AboutCtrl
  * Controller of the about view
  */
-angular.module('controllers.about', ['api.websocket'])
-  .controller('AboutCtrl', function() {
-    // TODO: add stats
+angular.module('controllers.about', ['countTo', 'api.websocket'])
+  .controller('AboutCtrl', function($scope, api) {
+
+    api.subscribe("twitchcancer.status", function(json) {
+      $scope.status = json;
+    });
+
+    $scope.$on("$destroy", function() {
+      api.unsubscribe("twitchcancer.status");
+    });
+
   })
 ;
